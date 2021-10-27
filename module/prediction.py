44acc0cd61757f23c0595ccaf0bfcf422263accb
Pierre Osselin <pierre.osselin@gmail.com>
2021-10-27 23:00:57 +0100

    Refactor Voting

--- module/prediction.py
+++ module/prediction.py
@@ -97,15 +97,15 @@ def sample_perturbed_graphs(data_idx, pf_minus, pf_plus, n, m, undirected, nsamp
     return per_data_idx
 
 
-def predict_smooth_gnn(attr_idx, edge_idx, pf, model, n, d, nc, n_samples, batch_size=1):
-    ### Raise error for value perturbation parameter
+def predict_smooth_model(attr_idx, edge_idx, pf, model, n, d, nc, n_samples, batch_size=1):
+
     model.eval()
     votes = torch.zeros((n, nc), dtype=torch.long, device=edge_idx.device)
     with torch.no_grad():
         assert n_samples % batch_size == 0
         nbatches = n_samples // batch_size
         for _ in tqdm(range(nbatches)):
-            edge_idx_batch = sparse_perturb_multiple(data_idx=edge_idx, n=n, m=n, undirected=True,
+            edge_idx_batch = sample_perturbed_graphs(data_idx=edge_idx, n=n, m=n, undirected=True,
                                                pf=pf, nsamples=batch_size, offset_both_idx=True)
             attr_idx_batch = copy_idx(idx=attr_idx, dim_size=n, ncopies=batch_size, offset_both_idx=False)
             predictions = model(attr_idx=attr_idx_batch, edge_idx=edge_idx_batch,
@@ -113,3 +113,93 @@ def predict_smooth_gnn(attr_idx, edge_idx, pf, model, n, d, nc, n_samples, batch
             preds_onehot = F.one_hot(predictions, int(nc)).reshape(batch_size, n, nc).sum(0)
             votes += preds_onehot
     return votes.cpu().numpy()
+
+
+def predict_smooth_gnn_classification_community(loader, sample_config, model, n, d, nc, batch_size=1):
+    """
+    Predict smooth gnn for classification
+    Parameters
+    ----------
+    loader: torch_geometric.data.DataLoader
+        Data Loader for graphs dataset. Must contain a community indexing in it
+    sample_config: dict
+        Configuration specifying the sampling probabilities
+    model : torch.nn.Module
+        The GNN model.
+    n : int
+        Number of graphs
+    d : int
+        Number of features
+    nc : int
+        Number of classes
+    batch_size : int
+        Number of graphs to sample per batch (for vote evaluation)
+
+    Returns
+    -------
+    votes : array-like [n, nc]
+        The votes per class for each graph.
+    """
+
+    n_samples = sample_config.get('n_samples', 0)
+    alpha = sample_config.get('parameter', 0)
+    dataset = sample_config.get('dataset', 0)
+    model.eval()
+
+    votes = torch.zeros((n, nc), dtype=torch.long, device=next(model.parameters()).device)
+
+    with torch.no_grad():
+
+        assert n_samples % batch_size == 0
+
+        # n_samples : number of samples for votes
+        # batch_size : number of these samples forwarded in gnn model at a time
+        nbatches = n_samples // batch_size
+
+        # Loop over the graphs in the dataset
+        for i, data in enumerate(loader):
+
+            #Get the graph structure and attributes
+            edge_idx = data.edge_index.cuda()
+            x = data.x.cuda()
+            n_graph = x.shape[0]
+            x_batch = x.repeat(batch_size, 1)
+            batch_idx = torch.arange(batch_size, device=edge_idx.device).repeat_interleave(n_graph, dim=0)
+            node_community = data.node_community
+            community_node = [torch.tensor(el).clone().detach().cuda() for el in data.community_node[0]]
+            community_size = data.community_size
+            community_prob = alpha*data.community_prob
+
+            # Loop over the perturbation graph batches
+            for _ in range(nbatches):
+
+                ### First function: should output a batch of perturbed graph the size
+                # of batch_size. Including x_batch, edge_idx_batch and batch_idx.
+                # Could improve by copying x
+                edge_idx_batch = sample_multiple_graphs_classification_community(edge_idx=edge_idx, sample_config = sample_config,
+                    node_community=node_community, community_node=community_node, community_size=community_size, community_prob=community_prob,
+                    nsamples=batch_size, n_nodes=n_graph)
+                
+                if dataset == "synthetic":
+                    ### Recompute the features
+                    ## With feature recomputation
+                    #adj = skn.utils.edgelist2adjacency(np.array(edge_idx_batch.cpu()).T)
+                    #betweenness = skn.ranking.Katz()
+                    #scores = betweenness.fit_transform(adj)
+                    G = nx.Graph()
+                    G.add_nodes_from(range(x_batch.shape[0]))
+                    G.add_edges_from(np.array(edge_idx_batch.cpu()).T)
+                    d2 = nx.algorithms.cluster.clustering(G)
+                    x_batch[:, 0] = torch.tensor([d2[i] for i in range(n_graph)])
+                    #x_batch[:, 0] = torch.tensor(scores)
+
+                predictions = model(x=x_batch, edge_index=edge_idx_batch,
+                                    batch=batch_idx).argmax(1)
+                preds_onehot = F.one_hot(predictions.to(torch.int64), int(nc)).sum(0)
+
+                votes[i] += preds_onehot
+
+            if i%10 == 0:
+                print(f'Processed {i}/{n} graphs')
+    return votes.cpu().numpy()
+
--- script/voting.py
+++ script/voting.py
@@ -3,7 +3,7 @@ import yaml
 import torch
 from module.models import load_model
 from torch_geometric.data import DataLoader
-from module.prediction import sample_perturbed_graphs
+from module.prediction import predict_smooth_model
 
 def vote(config):
 
@@ -11,55 +11,21 @@ def vote(config):
     l_data_test = torch.load(config["save_path"] + "dataset_train")
 
     # Load config parameters
-    nc, d, n = config["n_classes"], l_data_test[0].x.shape[1], len(l_data_test)
+    hidden_layers, n_class, d_features, n_data = config["hidden_channels"], config["n_classes"], l_data_test[0].x.shape[1], len(l_data_test)
     loader_test = DataLoader(l_data_test, batch_size = 1, shuffle = False)
 
-    model = load_model(config["model"])(hidden_channels=config["hidden_channels"], n_features=l_data_test[0].x.shape[1], n_classes=config["n_classes"]).cuda()
+    model = load_model(config["model"])(hidden_channels=hidden_layers, n_features=d_features, n_classes=n_class).cuda()
     model.load_state_dict(torch.load(config["weight_path"]))
 
-    n_samples_eval = config["sample_eval"]
-    n_samples_pre_eval = config["sample_pre_eval"]
-    batch_size = config["batch_size"]
-    path_votes = config["path_votes"]
+    n_samples_eval = config["sample_eval"] # Number of samples to predict label probabilities
+    n_samples_pre_eval = config["sample_pre_eval"] # Number of samples to predict label class
+    batch_size = config["batch_size"] # Number of graph perturbations before prediction
+    path_votes = config["path_votes"] # Path save votes
 
-    lp = config["lp"]
-    n_samples = sample_config.get('n_samples', 1)
-    dataset = sample_config.get('dataset', 1)
-
-    model.eval()
+    lp = config["lp"] # List of parameters to try
     votes = torch.zeros((n, nc), dtype=torch.long, device=next(model.parameters()).device)
-    with torch.no_grad():
 
-        assert n_samples % batch_size == 0
-        # n_samples : number of samples for votes
-        # batch_size : number of these samples forwarded in gnn model at a time
-        nbatches = n_samples // batch_size
-
-        # Loop over the graphs in the dataset
-        for i, data in enumerate(loader_test):
-            #Get the graph structure and attributes
-            edge_idx = data.edge_index.cuda()
-            x = data.x.cuda()
-            n_graph = x.shape[0]
-            x_batch = x.repeat(batch_size, 1)
-            batch_idx = torch.arange(batch_size, device=edge_idx.device).repeat_interleave(n_graph, dim=0)
-            # Loop over the perturbation graph batches
-            for _ in range(nbatches):
-
-                ### First function: should output a batch of perturbed graph the size
-                # of batch_size. Including x_batch, edge_idx_batch and batch_idx.
-                # Could improve by copying x
-                edge_idx_batch = sample_perturbed_graphs(
-                        edge_idx=edge_idx,
-                        sample_config=sample_config, nsamples=batch_size, n_nodes = n_graph)
-                
-                predictions = model(x=x_batch, edge_index=edge_idx_batch,
-                                    batch=batch_idx).argmax(1)
-                preds_onehot = F.one_hot(predictions.to(torch.int64), int(nc)).sum(0)
-                votes[i] += preds_onehot
-
-            if i%10 == 0:
-                print(f'Processed {i}/{n} graphs')
+    predict_smooth_model(attr_idx, edge_idx, pf, model, n, d, nc, n_samples, batch_size=1)
             
     return votes.cpu().numpy()
 
