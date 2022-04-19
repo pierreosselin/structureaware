e1010d698a3aacd0d909f666f67fd9c59d6a2e6b
Henry Kenlay <henrykenlay@hotmail.com>
2022-04-19 17:26:49 +0100

    changes to train model

--- script/train_model.py
+++ script/train_model.py
@@ -1,54 +1,24 @@
 import argparse
 
+import torchmetrics
 import torch
 import yaml
 from communityaware.models import GCN_Classification
 from os.path import join
-from communityaware.data import Synthetic
+from communityaware.data import Synthetic, HIV
 import os
-import torchmetrics
 import numpy as np
 
 
+# Parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--config', type=str, default='synthetic')
+parser.add_argument('--device', type=str, default='cpu')
+args = parser.parse_args()
+config = yaml.safe_load(open(f'config/{args.config}.yaml'))
+device = args.device
 
-def train_model(max_epochs, lr, hidden_channels, batch_size, data_path, model_path, device):
-
-    # Load data
-    dataset = Synthetic(data_path)
-    dataset.data.to(device)
-    train_loader = dataset.dataloader('train', batch_size)
-    valid_loader = dataset.dataloader('valid', batch_size)
-    test_loader = dataset.dataloader('test', batch_size)
-
-    # Instanciate model
-    model = GCN_Classification(num_features=dataset.num_features, hidden_channels=hidden_channels, num_classes=dataset.num_classes).to(device)
-    optimiser = torch.optim.Adam(model.parameters(), lr=lr)
-    criterion = torch.nn.CrossEntropyLoss(reduction='none')
-
-    # early stopping
-    best_model = None
-    best_loss = np.inf
-
-    # training
-    for epoch in range(max_epochs):
-        train_loss, train_accuracy = train_epoch(model, train_loader, criterion, optimiser)
-        valid_loss, valid_accuracy = evaluate(model, valid_loader, criterion)
-        print(epoch, round(train_loss, 3), round(valid_loss, 3), round(train_accuracy, 2), round(valid_accuracy, 2))
-        if valid_loss < best_loss:
-            best_loss = valid_loss
-            best_model = model.state_dict()
-
-    # test model
-    model.load_state_dict(best_model)
-    test_loss, test_accuracy = evaluate(model, test_loader, criterion)
-    print(f'Test loss: {round(test_loss, 3)}. Test accuracy: {round(test_accuracy, 2)}.')
-
-    # save model
-    print("Saving model...")
-    os.makedirs(model_path, exist_ok=True)
-    torch.save(model.state_dict(), join(model_path, "weights.pt"))
-
-
+# helper functions 
 def train_epoch(model, train_loader, criterion, optimiser):
     model.train()
     accuracy = torchmetrics.Accuracy()
@@ -75,18 +45,43 @@ def evaluate(model, loader, criterion):
             accuracy.update(out, batch.y)
         return epoch_loss.compute().item(), accuracy.compute().item()
 
+# load data
+if config['dataset'].lower() == 'synthetic':
+    dataset = Synthetic('data')
+elif config['dataset'].lower() == 'hiv':
+    dataset = HIV('data', min_required_edge_flips=20)
+dataset.data.to(device)
 
-if __name__ == '__main__':
-    # Argument definition
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--config', type=str, default='synthetic')
-    parser.add_argument('--device', type=str, default='cpu')
-    args = parser.parse_args()
+# dataloaders
+train_loader = dataset.dataloader('train', config['optimisation']['batch_size'])
+valid_loader = dataset.dataloader('valid', config['optimisation']['batch_size'])
+test_loader = dataset.dataloader('test', config['optimisation']['batch_size'])
 
-    config = yaml.safe_load(open(f'config/{args.config}.yaml'))
-    device = args.device
-    data_path = 'data'
-    model_path = join('output', config['dataset'], 'weights')
+# Instanciate model, optimiser and loss
+model = GCN_Classification(num_features=dataset.num_features, hidden_channels=config['optimisation']['hidden_channels'], num_classes=dataset.num_classes).to(device)
+optimiser = torch.optim.Adam(model.parameters(), lr=config['optimisation']['lr'])
+criterion = torch.nn.CrossEntropyLoss(reduction='none')
 
-    train_model(config['optimisation']['max_epochs'], config['optimisation']['lr'], config['optimisation']['hidden_channels'], config['optimisation']['batch_size'], data_path, model_path, device)
+# early stopping
+best_model = None
+best_loss = np.inf
 
+# training
+for epoch in range(config['optimisation']['max_epochs']):
+    train_loss, train_accuracy = train_epoch(model, train_loader, criterion, optimiser)
+    valid_loss, valid_accuracy = evaluate(model, valid_loader, criterion)
+    print(epoch, round(train_loss, 3), round(valid_loss, 3), round(train_accuracy, 2), round(valid_accuracy, 2))
+    if valid_loss < best_loss:
+        best_loss = valid_loss
+        best_model = model.state_dict()
+
+# test model
+model.load_state_dict(best_model)
+test_loss, test_accuracy = evaluate(model, test_loader, criterion)
+print(f'Test loss: {round(test_loss, 3)}. Test accuracy: {round(test_accuracy, 2)}.')
+
+# save model
+print("Saving model...")
+model_path = join('output', config['dataset'])
+os.makedirs(model_path, exist_ok=True)
+torch.save(model.state_dict(), join(model_path, "weights.pt"))
