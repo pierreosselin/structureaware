0017ce53e2d273516a85833d3de516e1ebe6de5e
Henry Kenlay <henrykenlay@hotmail.com>
2022-06-16 14:43:51 +0100

    added options

--- script_kernel/data.py
+++ script_kernel/data.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import pickle
 
@@ -6,6 +7,14 @@ import networkx as nx
 import numpy as np
 from sklearn.model_selection import train_test_split
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--motif_n', type=int, default=15)
+parser.add_argument('--random_n', type=int, default=17)
+parser.add_argument('--train_n', type=int, default=1000)
+parser.add_argument('--valid_n', type=int, default=1000)
+parser.add_argument('--test_n', type=int, default=10)
+args = parser.parse_args()
+
 
 def make_er_motif(n, motif):
     while True:
@@ -13,7 +22,7 @@ def make_er_motif(n, motif):
         connected = nx.is_connected(g)
         if connected and g.has_edge(0, 1):
             break
-    g = nx.relabel_nodes(g, {i: i+3 for i in range(n)})
+    g = nx.relabel_nodes(g, {i: i+len(motif)-2 for i in range(n)})
     g.add_edges_from(motif.edges())
     nx.set_node_attributes(g, {i: 1 for i in g.nodes()}, 'node_label')
     nx.set_edge_attributes(g, {i: 1 for i in g.edges()}, 'edge_label')
@@ -22,22 +31,28 @@ def make_er_motif(n, motif):
 if __name__ == '__main__':
 
     # specify the two motifs
-    motif1 = nx.cycle_graph(5)
-    motif2 = nx.complete_graph(5)
+    motif1 = nx.cycle_graph(args.motif_n)
+    motif2 = nx.complete_graph(args.motif_n)
 
     # generate the dataset
     dataset = []
     labels = []
-    for _ in range(305):
-        dataset.append(make_er_motif(7, motif1))
-        dataset.append(make_er_motif(7, motif2))
+    total_n = args.train_n + args.valid_n + args.test_n
+    for _ in range(int(total_n/2)):
+        dataset.append(make_er_motif(args.random_n, motif1))
+        dataset.append(make_er_motif(args.random_n, motif2))
         labels.append(0)
         labels.append(1)
     labels = np.array(labels)
     dataset = list(grakel.graph_from_networkx(dataset, node_labels_tag='node_label', edge_labels_tag='edge_label'))
-    G_train, G_valid_test, y_train, y_valid_test = train_test_split(dataset, labels, train_size=50, random_state=42, stratify=labels)
-    G_test, G_valid, y_test, y_valid = train_test_split(G_valid_test, y_valid_test, train_size=10, random_state=42, stratify=y_valid_test) # train size here is actually test size.
-    assert np.sum(y_test)==5
+    G_train, G_valid_test, y_train, y_valid_test = train_test_split(dataset, labels, train_size=args.train_n, stratify=labels)
+    G_test, G_valid, y_test, y_valid = train_test_split(G_valid_test, y_valid_test, train_size=args.test_n, stratify=y_valid_test) # train size here is actually test size.
+
+    assert len(G_train) == args.train_n
+    assert len(G_valid) == args.valid_n
+    assert len(G_test) == args.test_n
+    assert np.sum(y_test)==int(args.test_n/2)
+
 
     os.makedirs('data/kernel', exist_ok=True)
     output = {
@@ -48,4 +63,6 @@ if __name__ == '__main__':
         'y_valid': y_valid,
         'y_test': y_test
     }
-    pickle.dump(output, open('data/kernel/data.pkl', 'wb'))
+
+    fname = f'{args.motif_n}_{args.random_n}_{args.train_n}_{args.valid_n}_{args.test_n}'
+    pickle.dump(output, open(f'data/kernel/{fname}.pkl', 'wb'))
